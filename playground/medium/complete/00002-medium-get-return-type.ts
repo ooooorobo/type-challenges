24fcd9045f148a569c87c59483d337082a78fb45
조예진 <orobos654@gmail.com>
2023-07-08 21:28:45 +0900

    2-get return type

--- playground/medium/complete/00002-medium-get-return-type.ts
+++ playground/medium/complete/00002-medium-get-return-type.ts
@@ -24,12 +24,10 @@
 */
 
 /* _____________ 여기에 코드 입력 _____________ */
-
-type MyReturnType<T> = any
+type MyReturnType<T extends (...args: never[]) => unknown> = T extends (...args: never[]) => infer P ? P : never
 
 /* _____________ 테스트 케이스 _____________ */
 import type { Equal, Expect } from '@type-challenges/utils'
-
 type cases = [
   Expect<Equal<string, MyReturnType<() => string>>>,
   Expect<Equal<123, MyReturnType<() => 123>>>,
@@ -47,6 +45,7 @@ type ComplexObject = {
 }
 
 const fn = (v: boolean) => v ? 1 : 2
+// @ts-expect-error
 const fn1 = (v: boolean, w: any) => v ? 1 : 2
 
 /* _____________ 다음 단계 _____________ */
